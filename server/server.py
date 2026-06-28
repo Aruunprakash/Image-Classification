725cbfe8dcd32f4e5886012ad66fe158e4449b83
Aruunprakash <aruunmp@gmail.com>
2026-06-28 11:17:20 +0530

    Ready for deployment
    LiveReview Pre-Commit Check: skipped

--- server/server.py
+++ server/server.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import util
 
 app = Flask(__name__)
+CORS(app)
 
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 @app.route('/classify_image', methods=['GET', 'POST'])
 def classify_image():
